8c0386eb15f5b016a1311af1ac6540789f320654
WillDevAC <wydemairomsilva@gmail.com>
2020-08-30 15:00:16 -0300

    Neural Network

---
+++ index.py
@@ -0,0 +1,74 @@
+#REDE NEURAL - TAXA DE APRENDIZADO
+#Trabalho criado para estudos de IA - Aprendizado de redes
+#Feito por @WILL_DEVELOPER
+
+
+#Biblioteca principal
+import numpy as np
+
+
+
+#classe principal
+class Index(object):
+    def __init__(self, taxa_de_apredizagem=0.05, max_epocas=100, vies=1, peso_do_vies=0, x_pesos=None):
+        self.__taxa_de_apredizagem = taxa_de_apredizagem
+        self.__max_epocas = max_epocas
+        self.__vies = vies
+        self.__peso_do_vies = peso_do_vies
+        self.__x_pesos = x_pesos
+    
+
+    #pesos - vies
+    def peso_do_vies(self):
+        return self.__peso_do_vies
+    
+    #acertos
+    def x_pesos(self):
+        return self.__x_pesos
+    
+    #percentual
+    def percentual_de_acertos(self):
+        return self.__percentual_de_acertos
+
+    #processamento e treinamento    
+    def treinar_rede(self, X_treino, y_treino):
+        self.__x_pesos = np.zeros(X_treino.shape[1])
+        
+        for i in range(len(X_treino)):
+            n_epocas = 0
+            
+            while n_epocas < self.__max_epocas: 
+                u = self.__peso_do_vies * self.__vies
+
+                for j in range(len(self.__x_pesos)):
+                    u += self.__x_pesos[j] * X_treino[i][j]
+
+                y = 1 if u >= 0 else 0
+                e = y_treino[i] - y
+                
+                if e == 0:
+                    break
+
+                self.__peso_do_vies += self.__taxa_de_apredizagem * e * self.__vies
+                
+                for j in range(len(self.__x_pesos)):
+                    self.__x_pesos[j] += self.__taxa_de_apredizagem * e * X_treino[i][j]
+                    
+                n_epocas += 1
+    #teste - EQM          
+    def teste(self, X_teste, y_teste):
+        n_erros = 0
+
+        for i in range(len(X_teste)):
+            u = self.__peso_do_vies * self.__vies
+
+            for j in range(len(self.__x_pesos)):
+                u += self.__x_pesos[j] * X_teste[i][j]
+
+            y = 1 if u >= 0 else 0
+            e = y_teste[i] - y
+
+            if e != 0:
+                n_erros += 1
+
+        self.__percentual_de_acertos = 1 - n_erros / len(X_teste)
